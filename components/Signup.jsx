a38afbe26a095166ee81032b1d2c00328e5f192a
mehedishishir0 <mehedihasanshishir@gmail.com>
2025-07-06 18:17:04 +0600

    Initial commit

---
+++ components/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className=''>
+         <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 '>
+            <h1 className='text-3xl font-bold text-center text-gray-300'>Sign up</h1>
+         </div>
+    </div>
+  )
+}
+
+export default Signup
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Signup from '../components/Signup'
+import Login from '../components/Login'
+import Home from '../components/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+  },
+  {
+    path: '/register',
+    element: <Signup />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+])
+const App = () => {
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <RouterProvider router={router} />
+    </div>
+  )
+}
+
+export default App
